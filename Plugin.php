654ec3311c9df180afee6fe553344519522b868b
Joel E. Svensson <joeles_93@hotmail.com>
2016-07-29 22:57:26 +0200

    First working version

---
+++ Plugin.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace JoelESvensson\OcTrustedProxy;
+
+use Fideloper\Proxy\TrustProxies;
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+    public function pluginDetails()
+    {
+        return array(
+            'name'        => 'Reverse Proxy',
+            'description' => '',
+            'author'      => 'Joel E. Svensson',
+            'icon'        => 'icon-server',
+            'homepage'    => ''
+        );
+    }
+
+    public function register()
+    {
+        $this->app->singleton(
+            TrustProxies::class,
+            function ($app) {
+                $config = $app->make('config');
+                $proxies = env('TRUSTED_PROXIES');
+                if ($proxies) {
+
+                    /**
+                     * Create array of IP:s from comma separated string
+                     */
+                    $proxies = collect(explode(',', $proxies))
+                        ->map(function ($item) {
+                            return trim($item);
+                        })
+                    ;
+                } else {
+                    $proxies = [];
+                }
+                $config->set('trustedproxy.proxies', $proxies);
+                return new TrustProxies($config);
+            }
+        );
+    }
+
+    public function boot()
+    {
+        $this->app[\Illuminate\Contracts\Http\Kernel::class]
+            ->prependMiddleware(TrustProxies::class)
+        ;
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# October Trusted Proxy Plugin
